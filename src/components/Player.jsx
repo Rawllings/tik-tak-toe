68f6639763b678ea12b7c15adf3a0767dc9ad985
Rawllings Kiprop <rutojr01@gmail.com>
2024-06-11 17:37:35 +0300

    Lifting the state up for changing the player turns and board symbols

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function Player({ initialName, symbol }) {
+function Player({ initialName, symbol, isActive }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
@@ -22,7 +22,7 @@ function Player({ initialName, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol"> {symbol} </span>
