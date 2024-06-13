d1ccc8da3b6aa2152e402e9a7769e32735faa4cf
Rawllings Kiprop <rutojr01@gmail.com>
2024-06-13 12:13:13 +0300

    Done with the game

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 
-function Player({ initialName, symbol, isActive }) {
+function Player({ initialName, symbol, isActive, onChangeName }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
 
   const handleEditClick = () => {
     setIsEditing((editing) => !editing);
+    
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   };
 
   const handleChange = (e) => {
